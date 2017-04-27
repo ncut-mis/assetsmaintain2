067133b7a8325422fd89580c8e86cf17b403deee
S2092752\SSU-CHIEH <baby0925862059@gmail.com>
2017-04-27 14:48:41 +0800

    更新使用者與耗材路徑

--- routes/web.php
+++ routes/web.php
@@ -26,9 +26,19 @@ Auth :: routes();
  //用戶路徑
 Route :: get(' / users '           ,[ ' as '  =>  ' User.index ',' uses '  =>  ' UsersController @ index ' ]); //列出所有使用者
 Route :: get(' / users / create '    ,[ ' as '  =>  ' User.create ',' uses '  =>  ' UsersController @ create ' ]); //抓出新增使用者表單1-1（新增）
-Route :: post(' / users '          ,[ ' as '  =  'User.store','uses'  =>  'UsersController @ store' ]);  //儲存使用者1-資料2-1（修改）
-Route :: patch('/users/{id} '    ,[ ' as '  =>  ' User.update ',' uses '  =>  ' UsersController @ update ' ]); //更新資料2-2
-Route :: delete('/users/{id}'  ,[ ' as '  =>  ' User.destroy ',' uses '  =>  ' UsersController @ destroy ' ]); //刪除
+Route :: post(' / users '          ,[ ' as ' =>  'User.store','uses'  =>  'UsersController @ store' ]);  //儲存使用者1-2
+Route :: edit(' / users/{id}'          ,[ ' as ' =>  'User.store','uses'  =>  'UsersController @ store' ]); //抓出表單2-1（修改)
+Route :: patch('/users/{id} '    ,[ ' as '  =>  ' User.update ',' uses '  =>  ' UsersController @ update ' ]); //更新使用者資料2-2
+Route :: delete('/users/{id}'  ,[ ' as '  =>  ' User.destroy ',' uses '  =>  ' UsersController @ destroy ' ]); //刪除使用者
+Route :: get('/users/query'  ,[ ' as '  =>  ' User.query  ',' uses '  =>  ' UsersController @ query ' ]); //查詢使用者
+//耗材路徑
+Route :: get(' /supplies '           ,[ ' as '  =>  ' Supply.index ',' uses '  =>  ' SuppliesController @ index ' ]); //列出所有使用者
+Route :: get(' / supplies / create '    ,[ ' as '  =>  ' Supply.create ',' uses '  =>  'SuppliesController @ create ' ]); //抓出新增使用者表單1-1（新增）
+Route :: post(' /supplies '          ,[ ' as ' =>  'Supply.store','uses'  =>  'SuppliesController @ store' ]);  //儲存使用者1-2
+Route :: edit(' / supplies/{id} '          ,[ ' as ' =>  'Supply.edit','uses'  =>  'SuppliesController @ edit' ]); //抓出表單2-1（修改）
+Route :: patch('/supplies/{id} '    ,[ ' as '  =>  'Supply.update ',' uses '  =>  ' SuppliesController @ update ' ]); //更新資料2-2
+Route :: delete('/supplies/{id}'  ,[ ' as '  =>  ' Supply.destroy ',' uses '  =>  ' SuppliesController @ destroy ' ]); //刪除
+Route :: get('/supplies/query'  ,[ ' as '  =>  ' Supply.query  ',' uses '  =>  ' SuppliesController @ query ' ]); //查詢使用者
 
 
 
